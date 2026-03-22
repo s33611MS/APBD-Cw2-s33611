9f7f12d4d2cb4e9a316a19c5000ba3cd22f696d8
s33611MS <s33611@pjwstk.edu.pl>
2026-03-22 18:33:10 +0100

    feat(user): add user types

---
+++ APBD2/Models/Users/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD2.Models.Users;
+
+public class Employee(string name, string surname) : User(name, surname, "Employee")
+{
+    public override int GetMaxRented() => 5;
+}
---
+++ APBD2/Models/Users/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD2.Models.Users;
+
+public class Student(string name, string surname) : User(name, surname, "Student")
+{
+    public override int GetMaxRented() => 2;
+}
