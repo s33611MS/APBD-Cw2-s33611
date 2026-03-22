6cb158ed50a097562bb4a9200cedcd92724fe6c7
s33611MS <s33611@pjwstk.edu.pl>
2026-03-22 18:22:45 +0100

    feat(user): add base User class

---
+++ APBD2/Models/Users/User.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD2.Models.Users;
+
+public abstract class User(string name, string surname, string type)
+{
+    private static int _idGenerator = 1;
+    
+    public int Id { get; } = _idGenerator++;
+    public string Name { get; set; } = name;
+    public string Surname { get; set; } = surname;
+    public string Type { get; set; } = type;
+
+    public abstract int GetMaxRented();
+}
